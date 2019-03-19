93ec120b82333eb5e8ca439d66d3666142573904
Sam MacGinty <smacgint@nd.edu>
2019-03-19 14:02:52 -0400

    conversion works

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn import preprocessing
 from collections import defaultdict
 
+##IMPORT FAKE TWEETS AND DELETE UNEEDED COLUMNS##
+
 names = ["id","text","source","user_id","truncated","in_reply_to_status_id","in_reply_to_user_id","in_reply_to_screen_name","retweeted_status_id","geo","place","contributors","retweet_count","reply_count","favorite_count","favorited","retweeted","possibly_sensitive","num_hashtags","num_urls","num_mentions","created_at","timestamp","crawled_at","updated"]
 dataset = pandas.read_csv('tweets.txt', names=names)
 
@@ -27,7 +29,10 @@ del dataset['contributors']
 del dataset['truncated']
 del dataset['source']
 
+##ADD NEW COLUMN SAYING THEY ARE A BOT##
 dataset['is_bot'] = 1
+
+##IMPORT REAL TWEETS AND DELETE UNEEDED COLUMNS##
 
 names = ["id","text","source","user_id","truncated","in_reply_to_status_id","in_reply_to_user_id","in_reply_to_screen_name","retweeted_status_id","geo","place","contributors","retweet_count","reply_count","favorite_count","favorited","retweeted","possibly_sensitive","num_hashtags","num_urls","num_mentions","created_at","timestamp","crawled_at","updated"]
 real_dataset = pandas.read_csv('realtweets.txt', names=names)
@@ -48,29 +53,35 @@ del real_dataset['contributors']
 del real_dataset['truncated']
 del real_dataset['source']
 
+##ADD NEW COLUMN SAYING THEY ARE NOT A BOT##
 real_dataset['is_bot'] = 0
 
+#Merge datasets
 frames = [dataset,real_dataset]
-
 full = pandas.concat(frames)
 
+#Randomize order
 final = full.sample(frac=1)
 
-d = defaultdict(preprocessing.LabelEncoder)
+#Set columns we want to base prediction off
+feature_cols = ['retweet_count','reply_count','favorite_count']
 
-final.apply(preprocessing.LabelEncoder().fit_transform).astype(str)
+#Convert values in featured columns to floats
+for col in feature_cols:
+    final[col] = pandas.to_numeric(final[col], errors='coerce')
+    final = final.dropna(subset=[col])
+    final[col] = final[col].astype(float)
 
+#Split dataset and assign x and y values
 train, test = train_test_split(final, test_size=0.2)
-
-feature_cols = ['id', 'text', 'user_id', 'retweet_count','reply_count','favorite_count']
-
 X = train.loc[:, feature_cols]
 y = train.is_bot
 
+#Create and train model
 gnb = GaussianNB()
 gnb.fit(train[feature_cols].values, train["is_bot"])
 
+#Predict with model and print results
 y_pred = gnb.predict(test[feature_cols])
-
-print("Number of mislabeled points out of a total {} points : {}, performance {:05.2f}%".format(test.shape[0],(test["is_bot"] != y_pred).sum(),100*(1-(test["Survived"] != y_pred).sum()/test.shape[0])))
+print("Number of mislabeled points out of a total {} points : {}, performance {:05.2f}%".format(test.shape[0],(test["is_bot"] != y_pred).sum(),100*(1-(test["is_bot"] != y_pred).sum()/test.shape[0])))
 
